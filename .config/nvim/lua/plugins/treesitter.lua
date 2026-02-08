d523228dbcbc0a383018c365f891f9a1721a9f24
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2026-02-08 12:04:32 -0500

    fix: treesitter modules off of main
    They removed modules. But we can bring them back.
    It was removed so you can write your own easy APIs. IE no need to use
    nvim-treesitter to do it for you. But for now, this fix is good!

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,90 +5,89 @@ table.insert(M, {
     'nvim-treesitter/nvim-treesitter',
     branch = "main",
     version = false,
-    dependencies = {
-        {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            branch = "main",
-        }
-    },
     build = ':TSUpdate',
     lazy = false,
-    config = function()
-        -- See `:help nvim-treesitter`
-        require('nvim-treesitter').setup(
-            {
-                ensure_installed = {
-                    'c', 'cpp', 'lua', 'python', 'rust', 'tsx',
-                    'javascript', 'typescript', 'vimdoc', 'vim', 'bash',
-                    'markdown', 'markdown_inline', 'comment', 'gitignore', 'json',
-                    'toml', 'sql', 'requirements', 'yaml', 'xml', 'terraform',
-                    'gomod', 'gowork', 'gosum', 'go',
-                    'ruby', 'embedded_template',
-                },
-                auto_install = false,
+})
+-- Hand implement treesitter-modules with new nvim-treesitter API
+-- Waiting on a few things related to incremental selection
+table.insert(M, {
+    'MeanderingProgrammer/treesitter-modules.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    ---@module 'treesitter-modules'
+    ---@type ts.mod.UserConfig
+    opts = {
+        ensure_installed = {
+            'c', 'cpp', 'lua', 'python', 'rust', 'tsx',
+            'javascript', 'typescript', 'vimdoc', 'vim', 'bash',
+            'markdown', 'markdown_inline', 'comment', 'gitignore', 'json',
+            'toml', 'sql', 'requirements', 'yaml', 'xml', 'terraform',
+            'gomod', 'gowork', 'gosum', 'go',
+            'ruby', 'embedded_template',
+        },
+        auto_install = false,
 
-                highlight = {
-                    enable = true,
-                    -- regex highlighting is turned on for better spell checking.
-                    -- In the case of performance issues, turn this off
-                    additional_vim_regex_highlighting = true,
+        highlight = {
+            enable = true,
+            -- regex highlighting is turned on for better spell checking.
+            -- In the case of performance issues, turn this off
+            additional_vim_regex_highlighting = true,
+        },
+        indent = { enable = true },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = '<c-space>',
+                node_incremental = '<c-space>',
+                scope_incremental = '<c-s>',
+                node_decremental = '<M-space>',
+            },
+        },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ['aa'] = '@parameter.outer',
+                    ['ia'] = '@parameter.inner',
+                    ['af'] = '@function.outer',
+                    ['if'] = '@function.inner',
+                    ['ac'] = '@class.outer',
+                    ['ic'] = '@class.inner',
                 },
-                indent = { enable = true },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = '<c-space>',
-                        node_incremental = '<c-space>',
-                        scope_incremental = '<c-s>',
-                        node_decremental = '<M-space>',
-                    },
+            },
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    [']m'] = '@function.outer',
+                    [']]'] = '@class.outer',
                 },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ['aa'] = '@parameter.outer',
-                            ['ia'] = '@parameter.inner',
-                            ['af'] = '@function.outer',
-                            ['if'] = '@function.inner',
-                            ['ac'] = '@class.outer',
-                            ['ic'] = '@class.inner',
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            [']m'] = '@function.outer',
-                            [']]'] = '@class.outer',
-                        },
-                        goto_next_end = {
-                            [']M'] = '@function.outer',
-                            [']['] = '@class.outer',
-                        },
-                        goto_previous_start = {
-                            ['[m'] = '@function.outer',
-                            ['[['] = '@class.outer',
-                        },
-                        goto_previous_end = {
-                            ['[M'] = '@function.outer',
-                            ['[]'] = '@class.outer',
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ['<leader>a'] = '@parameter.inner',
-                        },
-                        swap_previous = {
-                            ['<leader>A'] = '@parameter.inner',
-                        },
-                    },
+                goto_next_end = {
+                    [']M'] = '@function.outer',
+                    [']['] = '@class.outer',
                 },
-            }
-        )
-    end
+                goto_previous_start = {
+                    ['[m'] = '@function.outer',
+                    ['[['] = '@class.outer',
+                },
+                goto_previous_end = {
+                    ['[M'] = '@function.outer',
+                    ['[]'] = '@class.outer',
+                },
+            },
+            -- TODO: swap logic was moved
+            -- Doesn't exist here anymore
+            swap = {
+                enable = true,
+                swap_next = {
+                    ['<leader>a'] = '@parameter.inner',
+                },
+                swap_previous = {
+                    ['<leader>A'] = '@parameter.inner',
+                },
+            },
+        },
+    }
 })
 return M
