e3d53690cdf3f8af07ca7659b4083b168528535b
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2026-07-26 15:35:40 -0400

    feat: better icons for explorer

---
+++ .config/nvim/lua/plugins/icons.lua
@@ -0,0 +1,13 @@
+-- Provide file icons for snacks.explorer
+return {
+    "nvim-mini/mini.icons",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+        package.preload["nvim-web-devicons"] = function()
+            require("mini.icons").mock_nvim_web_devicons()
+            return package.loaded["nvim-web-devicons"]
+        end
+    end,
+}
